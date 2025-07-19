d64bf0da1d0d92f6de59ddecaf960dd31ad27600
uzma-khan98 <uzma.uk.1998@gmail.com>
2025-07-19 15:11:39 +0500

    Added description of lecture in comments

--- JS/lecture-01.js
+++ JS/lecture-01.js
@@ -1,3 +1,8 @@
+// ! Lecture-01
+//? In this lecture, we are getting the understanding of how the Js code executes, behind the scene?Which phases are there involved in its code execution?
+// ? Where do the variables get their placeholders in Memory Heap? Then go to Execution phase...where they are actually assigned values. What happens with the Functions?
+
+
 var val1 = 10;
 var val2 = 5;
 function addNum(num1,num2) {
