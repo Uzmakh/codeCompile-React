c5f1ece91c2e367b73dcac5c85ca45b67e2e1b0f
uzma-khan98 <uzma.uk.1998@gmail.com>
2025-07-20 11:20:45 +0500

    Added JSX Attributes

--- react/dailytask/src/components/DailyTasks.jsx
+++ react/dailytask/src/components/DailyTasks.jsx
@@ -5,7 +5,9 @@ const DailyTasks = () => {
     // const countTask = () => {
     //     return task;
     // }
-    // use of if-else condition
+
+  
+  //* use of if-else condition
     const countTask = () => {
         // if (task === 0) {
         //     return "No task available!!";
@@ -14,8 +16,10 @@ const DailyTasks = () => {
         // }
        return task === 0 ? "No task available!!" : `Total tasks:  ${task}`; 
     }
-    // alternatively,use of ternary operator
+    //* alternatively,use of ternary operator
 
+  // * adding attribute to element
+  const isDisabled = true;
   return (
     <div>
           <h1>DailyTask</h1>
@@ -23,7 +27,7 @@ const DailyTasks = () => {
           <h2> {task}</h2>
           {/* using/calling function */}
           <h2> {countTask()}</h2>
-          <button>Add Task</button>
+          <button disabled={isDisabled} value="Add the task">Add Task</button>
     </div>
   )
 }
