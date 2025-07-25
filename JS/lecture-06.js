c50724a63a198b03ef4d6c01aee4a28137d8235e
uzma-khan98 <uzma.uk.1998@gmail.com>
2025-07-25 18:20:55 +0500

    uncommented the code

--- JS/lecture-06.js
+++ JS/lecture-06.js
@@ -1,17 +1,17 @@
 // ! Lecture-06
 // ? Closure in Javascript
 
-// ? outer function and inner function
-// function createCounter() {
-//     let count = 0;
-//     return function () {
-//         count++;
-//         return count;
-//     }
-// }
+//* outer function and inner function
+function createCounter() {
+    let count = 0;
+    return function () {
+        count++;
+        return count;
+    }
+}
 
-// const counter = createCounter();   //* we have inner function in our outer function,so it stores in the variable and we call it to get the values of the count
-// console.log(counter);
-// console.log(counter());
-// console.log(counter());
-// console.log(counter());
+const counter = createCounter();   //* we have inner function in our outer function,so it stores in the variable and we call it to get the values of the count
+console.log(counter);
+console.log(counter());   //* 1st call
+console.log(counter());    //* 2nd call
+console.log(counter());     //* 3rd call
