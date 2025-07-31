323231cb4badc43dbf4f60ff3cea6055930efa3e
uzma-khan98 <uzma.uk.1998@gmail.com>
2025-08-01 01:12:27 +0500

    Uncommented the code

--- JS/lecture-11.js
+++ JS/lecture-11.js
@@ -2,31 +2,31 @@
 // ? Shallow Copy & Deep Copy
 
 // * We have an object
-// const User = {
-//   name: "Umar",
-//   address: {
-//     city: "Wah",
-//     pinCode: 11066,
-//   },
-// };
+const User = {
+  name: "Umar",
+  address: {
+    city: "Wah",
+    pinCode: 11066,
+  },
+};
 
 //* I want to create its copy. There are 2 ways; shallow copy and deep copy;
 //* Shallow Copy: 2 methods to do this
 // ? Method-1:
-// const shallowCopy = Object.assign({}, User);
+const shallowCopy = Object.assign({}, User);
 // ? Method-2:
 // const shallowCopy = { ...User };
 //* Deep Copy
-// const deepCopy = JSON.parse(JSON.stringify(User));
+const deepCopy = JSON.parse(JSON.stringify(User));
 
-// * Now if I get any change in original object-User, whether this change will reflect in shallow copy
-// User.address.city = "Okara";
+// * Now if I get any change in original object-User, whether this change will reflect in shallow copy or not
+User.address.city = "Okara";
 
-// console.log("Shallow Copy :", shallowCopy);
-// console.log("Shallow Copy :", shallowCopy.address.city);
+console.log("Shallow Copy :", shallowCopy);
+console.log("Shallow Copy :", shallowCopy.address.city);
 // * the change in original will also reflects/updates in shallow bcz their memory reference is the same(they share the same memory reference)
 
-// console.log("Deep Copy :", deepCopy);
-// console.log("Deep Copy :", deepCopy.address.city);
+console.log("Deep Copy :", deepCopy);
+console.log("Deep Copy :", deepCopy.address.city);
 // * the change in original will not reflect/update the deep copy bcz their memory reference is different(they don't share the same memory reference)
 
