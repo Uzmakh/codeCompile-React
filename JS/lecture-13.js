65af00c8544d102a86b1177478dcb65c8b18797f
uzma-khan98 <uzma.uk.1998@gmail.com>
2025-08-03 17:12:27 +0500

    Uncommented the code

--- JS/lecture-13.js
+++ JS/lecture-13.js
@@ -11,10 +11,10 @@ const User2 = {
 }
 
 function printDetails(){
-    // console.log(`Name: ${this.name} Age:${this.age}`);
+     console.log(`Name: ${this.name}, Age:${this.age}`);
 }
 
-// printDetails();   //undefined
+//  printDetails();   //undefined
 
 //* this-keyword is not able to access any of the object keys; as the function printDetails is outside the object-User1 and/or User2
 //* to achieve our goal, of accessing object-values, we use call() or apply() method with this-keyword
@@ -29,12 +29,12 @@ printDetails.apply(User2, ["Kolkata", "India"]);
 //* These two methods invoke the function immediately, with the difference of having items in an array for apply() method
 //* Use any of these methods while calling a function to access object-values with this-keyword
 
-// printDetails(); //undefined
+printDetails(); //undefined
 
 //* There's still another method to call a function in this scenario; that is bind()
 
 const myDetails = printDetails.bind(User2, "AllahAbad", "India");
-// console.log(myDetails());
+console.log(myDetails());
 myDetails();
 
 //* bind() method does not invoke the function immediately,rather it returns a new function,storing it in a variable
