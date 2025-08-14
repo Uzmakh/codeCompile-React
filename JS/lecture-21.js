67ea76dc27dc8dcf8515209ea7cf533c87797887
uzma-khan98 <uzma.uk.1998@gmail.com>
2025-08-14 15:13:22 +0500

    Uncommented the code

--- JS/lecture-21.js
+++ JS/lecture-21.js
@@ -49,54 +49,31 @@ orderFood(true).then((res) => {
 })
 
 // ? Fetch API
-// fetch("https://jsonplaceholder.typicode.com/posts/1").then((response) => {
-//     console.log(response);
-//    return response.json()
-// }).then((data) => {
-//     console.log(data);
-// }).catch((error) => {
-//     console.log("Error is HERE...", error);
-// })
+fetch("https://jsonplaceholder.typicode.com/posts/1").then((response) => {
+    console.log(response);
+   return response.json()
+}).then((data) => {
+    console.log(data);
+}).catch((error) => {
+    console.log("Error is HERE...", error);
+})
 
 // ? Promise.all()
 // ? It is a method that takes an array of promises and returns a new promise, that resolves when all of them are resolved or rejects if any one fails.
 
-// const fetchUser1 = fetch("https://jsonplaceholder.typicode.com/posts/1").then(res => res.json());
-// const fetchUser2 = fetch("https://jsonplaceholder.typicode.com/posts/2").then(res => res.json());
-// const fetchUser3 = fetch("https://jsonplaceholder.typicode/posts/3").then(res => res.json());
+const fetchUser1 = fetch("https://jsonplaceholder.typicode.com/posts/1").then(res => res.json());
+const fetchUser2 = fetch("https://jsonplaceholder.typicode.com/posts/2").then(res => res.json());
+const fetchUser3 = fetch("https://jsonplaceholder.typicode/posts/3").then(res => res.json());
 
 
-// Promise.all([fetchUser1, fetchUser2, fetchUser3]).then((posts) => {
-//     // console.log("All posts loaded!!", posts);
-//     console.log("All posts loaded!!"),
-//         posts.forEach(post => console.log(post.title, post.body));
-// }).catch((err) => {
-//     console.log("Error fetching posts info!", err)
-// });
+Promise.all([fetchUser1, fetchUser2, fetchUser3]).then((posts) => {
+    // console.log("All posts loaded!!", posts);
+    console.log("All posts loaded!!"),
+        posts.forEach(post => console.log(post.title, post.body));
+}).catch((err) => {
+    console.log("Error fetching posts info!", err)
+});
 
 // ? This method allows us to handle multiple promises at once.It returns an array of results when all the promises are resolved. If any promise is rejected, then Promise.all immediately rejects with that error.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
