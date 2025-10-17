c366fe1343bbe7489bcc1dc55b7301a54829e36a
uzma-khan98 <uzma.uk.1998@gmail.com>
2025-10-17 12:32:26 +0500

    created sample data

---
+++ react/Module-4/jiraboard/src/components/data.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+const data = [
+  {
+    task: "sample",
+    status: "Ready For Development",
+    tags: ["DEV"],
+  },
+  {
+    task: "test",
+    status: "In Progress",
+    tags: ["QA"],
+  }
+];
