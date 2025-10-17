86ec037ec6ebecd9086418787f6225e0a50bedc1
uzma-khan98 <uzma.uk.1998@gmail.com>
2025-10-17 12:32:05 +0500

    keeping constants separate

---
+++ react/Module-4/jiraboard/src/constants/common.js
@@ -0,0 +1,10 @@
+export const tagStyles = {
+  DEV: { backgroundColor: "#27497eff", color: "#fff" },
+  QA: { backgroundColor: "#BF092F", color: "#fff" },
+  PO: { backgroundColor: "#3B9797", color: "#fff" }
+};
+export const tagKeyMap = {
+  DEV: "DEV",
+  QA: "QA",
+  "Product Owner": "PO"
+}
